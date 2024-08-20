7ab78702d19994665aae1bfc9b690dfb938e4d7e
IttikornPaennark <ittikornpaennark@gmail.com>
2024-08-20 19:49:55 +0700

    first commit

---
+++ test/Main.java
@@ -0,0 +1,21 @@
+package test;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(question1());
+        System.out.println(question2());
+        System.out.println(question3());
+    }
+
+    private static int question1() {
+        return 0;
+    }
+
+    private static int question2() {
+        return 0;
+    }
+
+    private static int question3() {
+        return 0;
+    }
+}
