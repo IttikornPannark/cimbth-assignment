f2b530f1d9b0369e546263ba0a6fc1eb13f9b2dc
IttikornPaennark <ittikornpaennark@gmail.com>
2024-08-22 16:56:57 +0700

    edit q2

--- test/Main.java
+++ test/Main.java
@@ -28,36 +28,32 @@ class Main {
     private static int question2(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
-        String s1 = "";
-        String s2 = "";
         int minLength = 0;
         int result = 0;
+        List<String> l1 = new ArrayList<>();
+        List<String> l2 = new ArrayList<>();
         for (int i = 0; i < v1.length; i++) {
             if (!v1[i].startsWith("0")) {
-                s1 += v1[i];
+                l1.add(v1[i]);
             }
         }
         for (int j = 0; j < v2.length; j++) {
             if (!v2[j].startsWith("0")) {
-                s2 += v2[j];
+                l2.add(v2[j]);
+
             }
         }
-        if (s1.length() < s2.length()) {
-            minLength = s1.length();
-        } else {
-            minLength = s2.length();
-        }
-        
         int i = 0;
+        if(l1.size() < l2.size()) {
+            minLength = l1.size();
+        } else {
+            minLength = l2.size();
+        }
         while (i < minLength) {
-            System.out.println(s1.charAt(i));
-            System.out.println(s2.charAt(i));
-            if (s1.charAt(i) < s2.charAt(i)) {
+            if (Integer.parseInt(l1.get(i)) < Integer.parseInt(l2.get(i))) {
                 result = -1;
-            } else if (s1.charAt(i) > s2.charAt(i)) {
+            } else if (Integer.parseInt(l1.get(i)) > Integer.parseInt(l2.get(i))) {
                 result = 1;
-            } else {
-                result = 0;
             }
             i++;
         }
